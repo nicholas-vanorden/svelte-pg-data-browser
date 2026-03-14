0340d8bd5bb8e98e783909448eab484d33edab32
Nick Van Orden <nicholas.vanorden83@gmail.com>
2026-03-14 15:50:21 -0600

    page load on error

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,10 @@ import { Customer } from "$lib/server/customer"
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
-    const customers = await Customer().getAll()
-
-    return { customers }
+    try {
+        const customers = await Customer().getAll()
+        return { customers }
+    } catch {
+        return { customers: [] }
+    }
 }
